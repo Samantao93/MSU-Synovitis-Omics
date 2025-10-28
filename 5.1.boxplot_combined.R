ead86bb01536ac0cc3787d54dc18c0b4ad0ac663
Samanta Ortuño Miquel <samanta.ortuno@gmail.com>
2025-10-28 11:41:09 +0100

    Update 5.1.boxplot_combined.R

--- 5.1.boxplot_combined.R
+++ 5.1.boxplot_combined.R
@@ -1,26 +1,28 @@
 for (i in c("Sinovitis ecográfica","Microcristales")){
-  path_sign=paste0('./results/',i,'/1. Estadística Básica/Boxplot por grupos/significativas/')
+  path_sign=paste0('/datos/sam_user/reuma/results/',i,'/1. Estadística Básica/Boxplot por grupos/significativas/')
   if (i == 'Sinovitis ecográfica'){
     estadistica <- read_excel(paste0("results/",i,"/1. Estadística Básica/estadistica_sinovitis.xlsx"))
   } else {
     estadistica <- read_excel(paste0("results/",i,"/1. Estadística Básica/estadistica_microcristales.xlsx"))
   }
-  estadistica<-filter(estadistica,col != 'leucocito')
-  estadistica_filter_05<-filter(estadistica,test>0.01 & test<0.05)
-  estadistica_filter_01<-filter(estadistica,test>0.001 & test<0.01)
-  estadistica_filter_001<-filter(estadistica,test<0.001)
-  
-  bands <- list(p_01_05 = estadistica_filter_05,
-                p_001_01 = estadistica_filter_01)
+  estadistica<-filter(estadistica,col != 'leucocito' & col != 'edad')
   
   
-  if (i == "Microcristales") {
-    bands$p_001 <- estadistica_filter_001
+  if (i == 'Sinovitis ecográfica'){
+    estadistica_filter_01<-filter(estadistica,test<=0.01)
+    estadistica_filter_05<-filter(estadistica,test>0.01 & test<=0.05)
+    bands <- list(p_05_01 = estadistica_filter_05,
+                  p_01 = estadistica_filter_01)
+  } else {
+    estadistica_filter_001<-filter(estadistica,test<=0.001)
+    estadistica_filter_05_001<-filter(estadistica,test>0.001 & test<=0.05)
+    bands <- list(p_05_001 = estadistica_filter_05_001,
+                  p_001 = estadistica_filter_001)
   }
   
   for(band in names(bands)){
     x <- bands[[band]]
-    n <- length(x)
+    n <- nrow(x)
     cols <- ceiling(sqrt(n))
     rows <- ceiling(n / cols)
     tile_str <- paste0(cols, "x", rows)
@@ -45,5 +47,3 @@ for (i in c("Sinovitis ecográfica","Microcristales")){
                 density = "300x300")
   }
 }
-
-
